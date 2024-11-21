527fe8b4a10ea14dbcf15a47455f7511c68420c0
avchocho <adelina.chocho98@gmail.com>
2024-11-21 14:50:45 -0500

    space

--- src/Screens/LostScreen.java
+++ src/Screens/LostScreen.java
@@ -24,23 +24,18 @@ public class LostScreen extends Screen {
         loseScreen = ImageLoader.load("lose_screen.png");
 
 
-        keyLocker.lockKey(Key.SPACE);
+       
         keyLocker.lockKey(Key.ESC);
     }
 
     @Override
     public void update() {
-        if (Keyboard.isKeyUp(Key.SPACE)) {
-            keyLocker.unlockKey(Key.SPACE);
-        }
+       
         if (Keyboard.isKeyUp(Key.ESC)) {
             keyLocker.unlockKey(Key.ESC);
         }
 
-        // if space is pressed, reset level. if escape is pressed, go back to main menu
-        if (Keyboard.isKeyDown(Key.SPACE) && !keyLocker.isKeyLocked(Key.SPACE)) {
-            playLevelScreen.resetLevel();
-        } else if (Keyboard.isKeyDown(Key.ESC) && !keyLocker.isKeyLocked(Key.ESC)) {
+     if (Keyboard.isKeyDown(Key.ESC) && !keyLocker.isKeyLocked(Key.ESC)) {
             playLevelScreen.goBackToMenu();
         }
     }
